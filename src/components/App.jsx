eb877f5227914f1e143adfe10542251db34854e2
Amir <vibelessyoung@gmail.com>
2025-11-22 15:38:00 +0330

    start create AnimeCards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
 import HeroSlider from "./HeroSection.jsx";
+import Cards from "./AnimeCards.jsx";
 function App() {
   return (
     <>
       <Header />
       <HeroSlider />
+      <Cards />
     </>
   );
 }
